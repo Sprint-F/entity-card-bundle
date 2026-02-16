6f0ec7b9f8142ddaa4b1c707264a4a82d2a1811f
Степанцев Альберт <albert.stepantsev@sprintf.ru>
2026-02-16 22:33:21 +0300

    + Attribute

---
+++ src/Attribute/Card.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Common\EntityCard\Attribute;
+
+use SprintF\Metadata\Mapping\Attribute\MetadataAttribute;
+
+/**
+ * Атрибут, применяющийся к классу сущности, которая будет отображаться в карточке.
+ */
+#[\Attribute(\Attribute::TARGET_CLASS)]
+class Card extends MetadataAttribute
+{
+    public function __construct(
+        /** @var string Отображаемое имя сущности: заголовок карточки */
+        public readonly string $entityLabel,
+    ) {
+    }
+
+    public function getKey(): string
+    {
+        return 'card';
+    }
+}
---
+++ src/Attribute/CardRow.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Common\EntityCard\Attribute;
+
+use SprintF\Metadata\Mapping\Attribute\MetadataAttribute;
+use SprintF\ValueObjects\Value\DefaultValue;
+
+/**
+ * Атрибут, применяемый к свойству или геттеру данных сущности.
+ * Задает параметры отображения строки карточки сущности.
+ */
+#[\Attribute(\Attribute::TARGET_PROPERTY | \Attribute::TARGET_METHOD)]
+class CardRow extends MetadataAttribute
+{
+    public function __construct(
+        /** @var ?string Отображаемое имя строки карточки сущности */
+        public readonly ?string $label = null,
+        /** @var int Порядок отображения строки в карточке сущности */
+        public readonly int $order = 1,
+        /** @var string Имя класса-значения для данных, отображаемых в строке карточки */
+        public readonly string $valueClass = DefaultValue::class,
+        /** @var bool Отображать ли строку, если значение данных пустое? */
+        public readonly bool $showIfEmpty = true,
+    ) {
+    }
+
+    public function getKey(): string
+    {
+        return 'row';
+    }
+}
