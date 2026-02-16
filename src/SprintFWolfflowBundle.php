38e45c0a5133b1967a999cb253864107bc827e25
Степанцев Альберт <albert.stepantsev@sprintf.ru>
2026-02-16 23:32:14 +0300

    ! services.yaml

--- src/SprintFWolfflowBundle.php
+++ src/SprintFWolfflowBundle.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace SprintF\Bundle\EntityCard;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class SprintFEntityCardBundle extends AbstractBundle
@@ -11,5 +13,10 @@ class SprintFEntityCardBundle extends AbstractBundle
     public function getPath(): string
     {
         return dirname(__DIR__);
+    }
+
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/services.yaml');
     }
 }
